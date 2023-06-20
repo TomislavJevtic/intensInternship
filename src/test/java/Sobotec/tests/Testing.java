aa1a2c3fbe62ab0576b790b0c6c7165b37d22b85
TomislavJevtic <jevtic.tomislaw@gmail.com>
2023-06-20 11:52:32 +0200

    Testing test added

---
+++ src/test/java/Sobotec/tests/Testing.java
@@ -0,0 +1,13 @@
+package Sobotec.tests;
+
+import org.testng.annotations.Test;
+
+public class Testing extends BaseTest {
+    @Test
+    public void creatingProject() {
+
+        loginPage.login("asasa","asasasas");
+
+    }
+
+}
