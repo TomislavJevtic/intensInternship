50c906dc8144ec37f28ada5ad5ffb26433a48ae0
TomislavJevtic <jevtic.tomislaw@gmail.com>
2023-06-21 09:23:43 +0200

    Project page added

---
+++ src/test/java/Sobotec/pages/ProjectPage.java
@@ -0,0 +1,26 @@
+package Sobotec.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProjectPage extends BasePage {
+
+    private By projectTab = By.xpath("//*[@id=\"root\"]/div[2]/div[1]/div[2]/div/div/div/ul/li[3]/div/div[1]/a");
+
+    private By overview = By.className("mainnav-menu-items.active");
+
+
+    public WebElement getProjectTab() {
+        return getDriver().findElement(projectTab);
+    }
+    public WebElement getOverview() {
+        return getDriver().findElement(overview);
+    }
+
+
+    public ProjectPage(WebDriver driver, WebDriverWait driverWait) {
+        super(driver, driverWait);
+    }
+}
--- src/test/java/Sobotec/tests/BaseTest.java
+++ src/test/java/Sobotec/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package Sobotec.tests;
 
 import Sobotec.pages.LoginPage;
+import Sobotec.pages.ProjectPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,6 +16,8 @@ public abstract class  BaseTest {
     protected WebDriverWait driverWait;
     protected LoginPage loginPage;
 
+    protected ProjectPage projectPage;
+
 
 
     public BaseTest() {
@@ -25,9 +28,10 @@ public abstract class  BaseTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(12));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         loginPage = new LoginPage(driver, driverWait);
+        projectPage = new ProjectPage(driver,driverWait);
 
 
     }
